6fa7aad5de08635677dfbbf7b3b26363a97073ab
scunningham13 <156953717+scunningham13@users.noreply.github.com>
2024-02-03 21:07:36 +0000

    Drew a snowman

--- script.js
+++ script.js
@@ -5,5 +5,13 @@ function setup(){
 function draw(){
   background(200);
   // add your drawing code here ...
-  
-}
+  ellipse (300, 100, 80);
+  ellipse (300, 192, 105);
+  ellipse (300, 315, 140);
+  ellipse (285, 88, 10);
+  ellipse (315, 88, 10);
+  line(width/2, 120, width/2 + 20, 110);
+  ellipse(300, 210, 10);
+  ellipse(300, 185, 10);
+  ellipse(300, 165, 10);
+  }
